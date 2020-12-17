94d938041498024cd03f3085bb66fa14fc273a20
Tony Arcieri <tony@iqlusion.io>
2020-12-17 09:48:39 -0800

    proto-build discrepancies
    I've checked in the changes I get when I run `proto-build` locally to
    show the discrepancies I get when I run it locally.

--- cosmos-sdk-proto/src/prost/cosmos.base.store.v1beta1.rs
+++ cosmos-sdk-proto/src/prost/cosmos.base.store.v1beta1.rs
@@ -1,3 +1,30 @@
+/// CommitInfo defines commit information used by the multi-store when committing
+/// a version/height.
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct CommitInfo {
+    #[prost(int64, tag="1")]
+    pub version: i64,
+    #[prost(message, repeated, tag="2")]
+    pub store_infos: ::std::vec::Vec<StoreInfo>,
+}
+/// StoreInfo defines store-specific commit information. It contains a reference
+/// between a store name and the commit ID.
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct StoreInfo {
+    #[prost(string, tag="1")]
+    pub name: std::string::String,
+    #[prost(message, optional, tag="2")]
+    pub commit_id: ::std::option::Option<CommitId>,
+}
+/// CommitID defines the committment information when a specific store is
+/// committed.
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct CommitId {
+    #[prost(int64, tag="1")]
+    pub version: i64,
+    #[prost(bytes, tag="2")]
+    pub hash: std::vec::Vec<u8>,
+}
 /// SnapshotItem is an item contained in a rootmulti.Store snapshot.
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct SnapshotItem {
@@ -32,31 +59,4 @@ pub struct SnapshotIavlItem {
     pub version: i64,
     #[prost(int32, tag="4")]
     pub height: i32,
-}
-/// CommitInfo defines commit information used by the multi-store when committing
-/// a version/height.
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct CommitInfo {
-    #[prost(int64, tag="1")]
-    pub version: i64,
-    #[prost(message, repeated, tag="2")]
-    pub store_infos: ::std::vec::Vec<StoreInfo>,
-}
-/// StoreInfo defines store-specific commit information. It contains a reference
-/// between a store name and the commit ID.
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct StoreInfo {
-    #[prost(string, tag="1")]
-    pub name: std::string::String,
-    #[prost(message, optional, tag="2")]
-    pub commit_id: ::std::option::Option<CommitId>,
-}
-/// CommitID defines the committment information when a specific store is
-/// committed.
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct CommitId {
-    #[prost(int64, tag="1")]
-    pub version: i64,
-    #[prost(bytes, tag="2")]
-    pub hash: std::vec::Vec<u8>,
 }
