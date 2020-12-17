16ddc09f76d99d1fa5ecb4f37b47c4169392510b
Tony Arcieri <tony@iqlusion.io>
2020-12-17 15:24:55 -0800

    cosmos-sdk-proto v0.2.0 + cosmos-tx v0.2.0-pre

--- cosmos-sdk-proto/src/lib.rs
+++ cosmos-sdk-proto/src/lib.rs
@@ -1,9 +1,11 @@
-//! cosmos-sdk-proto library gives the developer access to the Cosmos SDK proto-defined structs.
-//! These are then re-exported in a module structure as close as possible to the proto files. The
-//! version strings are intentionally excluded, that way users may specify cosmos-sdk versions as
-//! a feature argument to this crate and not have to change their imports. For as much as that is
+//! The `cosmos-sdk-proto` crate provides access to the Cosmos SDK proto-defined structs.
+//! These are then re-exported in a module structure as close as possible to the proto files.
+//!
+//! The version strings are intentionally excluded, that way users may specify `cosmos-sdk` versions
+//! as a feature argument to this crate and not have to change their imports. For as much as that is
 //! worth considering that modules seem to show up and go away with every RC.
-//! TODO actually implement features tag based compilation
+//!
+//! TODO: actually implement features tag based compilation
 
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![doc(
@@ -16,108 +18,150 @@
 /// The version (commit hash) of the Cosmos SDK used when generating this library.
 pub const COSMOS_SDK_VERSION: &str = include_str!("prost/COSMOS_SDK_COMMIT");
 
-/// Cosmos protobuf definitions
+/// Cosmos protobuf definitions.
 pub mod cosmos {
+    /// Authentication of accounts and transactions.
     pub mod auth {
         pub mod v1beta1 {
             include!("prost/cosmos.auth.v1beta1.rs");
         }
     }
+
+    /// Proof-of-Stake layer for public blockchains.
     pub mod staking {
         pub mod v1beta1 {
             include!("prost/cosmos.staking.v1beta1.rs");
         }
     }
+
+    /// Base functionality.
     pub mod base {
+        /// Application BlockChain Interface (ABCI).
+        ///
+        /// Interface that defines the boundary between the replication engine
+        /// (the blockchain), and the state machine (the application).
         pub mod abci {
             pub mod v1beta1 {
                 include!("prost/cosmos.base.abci.v1beta1.rs");
             }
         }
+
+        /// Key-value pairs.
         pub mod kv {
             pub mod v1beta1 {
                 include!("prost/cosmos.base.kv.v1beta1.rs");
             }
         }
+
+        /// Query support.
         pub mod query {
             pub mod v1beta1 {
                 include!("prost/cosmos.base.query.v1beta1.rs");
             }
         }
+
+        /// Reflection support.
         pub mod reflection {
             pub mod v1beta1 {
                 include!("prost/cosmos.base.reflection.v1beta1.rs");
             }
         }
+
+        /// Snapshots containing Tendermint state sync info.
         pub mod snapshots {
             pub mod v1beta1 {
                 include!("prost/cosmos.base.snapshots.v1beta1.rs");
             }
         }
+
+        /// Data structure that holds the state of the application.
         pub mod store {
             pub mod v1beta1 {
                 include!("prost/cosmos.base.store.v1beta1.rs");
             }
         }
+
         pub mod v1beta1 {
             include!("prost/cosmos.base.v1beta1.rs");
         }
     }
+
+    /// Cryptographic primitives.
     pub mod crypto {
+        /// Multi-signature support.
         pub mod multisig {
             pub mod v1beta1 {
                 include!("prost/cosmos.crypto.multisig.v1beta1.rs");
             }
         }
     }
+
+    /// Transactions.
     pub mod tx {
+        /// Transaction signing support.
         pub mod signing {
             pub mod v1beta1 {
                 include!("prost/cosmos.tx.signing.v1beta1.rs");
             }
         }
+
         pub mod v1beta1 {
             include!("prost/cosmos.tx.v1beta1.rs");
         }
     }
 }
 
-/// IBC protobuf definitions
+/// IBC protobuf definitions.
 pub mod ibc {
+    /// IBC applications.
     pub mod applications {
+        /// Transfer support.
         pub mod transfer {
             pub mod v1 {
                 include!("prost/ibc.applications.transfer.v1.rs");
             }
         }
     }
+
+    /// IBC core.
     pub mod core {
+        /// IBC channels.
         pub mod channel {
             pub mod v1 {
                 include!("prost/ibc.core.channel.v1.rs");
             }
         }
+
+        /// IBC client.
         pub mod client {
             pub mod v1 {
                 include!("prost/ibc.core.client.v1.rs");
             }
         }
+
+        /// IBC commitments.
         pub mod commitment {
             pub mod v1 {
                 include!("prost/ibc.core.commitment.v1.rs");
             }
         }
+
+        /// IBC connections.
         pub mod connection {
             pub mod v1 {
                 include!("prost/ibc.core.connection.v1.rs");
             }
         }
+
+        /// IBC types.
         pub mod types {
             pub mod v1 {
                 include!("prost/ibc.core.types.v1.rs");
             }
         }
     }
+
+    /// IBC light clients.
     pub mod lightclients {
         pub mod localhost {
             pub mod v1 {
@@ -137,7 +181,7 @@ pub mod ibc {
     }
 }
 
-/// ICS23 protobuf definitions
+/// ICS23 protobuf definitions.
 pub mod ics23 {
     include!("prost/ics23.rs");
 }
--- cosmos-tx/src/lib.rs
+++ cosmos-tx/src/lib.rs
@@ -2,7 +2,7 @@
 
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/cosmos/cosmos-rust/main/.images/cosmos.png",
-    html_root_url = "https://docs.rs/cosmos-sdk-proto/0.1.2"
+    html_root_url = "https://docs.rs/cosmos-sdk-proto/0.2.0-pre"
 )]
 #![forbid(unsafe_code)]
 #![warn(trivial_casts, trivial_numeric_casts, unused_import_braces)]
