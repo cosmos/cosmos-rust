3c6ead64f2453aed5e0f6b677bf056cf472de1a4
Justin Kilpatrick <justin@althea.net>
2021-04-08 16:49:01 -0400

    Add imports for secp256k1 and ed25519 proto defs (#73)
    I missed these in my previous comments, just a few minor key type
    structs.

--- cosmos-sdk-proto/src/lib.rs
+++ cosmos-sdk-proto/src/lib.rs
@@ -103,9 +103,16 @@ pub mod cosmos {
     pub mod crypto {
         /// Multi-signature support.
         pub mod multisig {
+            include!("prost/cosmos.crypto.multisig.rs");
             pub mod v1beta1 {
                 include!("prost/cosmos.crypto.multisig.v1beta1.rs");
             }
+        }
+        pub mod ed25519 {
+            include!("prost/cosmos.crypto.ed25519.rs");
+        }
+        pub mod secp256k1 {
+            include!("prost/cosmos.crypto.secp256k1.rs");
         }
     }
 
