2b4d19b120ceb5f06929a3cf24512d4f0df085c6
Tony Arcieri (iqlusion) <tony@iqlusion.io>
2021-10-28 11:19:33 -0700

    cosmrs: cosmos.crypto.multisig.LegacyAminoPubKey support (#147)
    Adds support for this key type when used as the `public_key` field of
    `SignerInfo`.
    
    To accomodate this, a new `SignerPublicKey` enum was added with variants
    for a single `PublicKey` or a `LegacyAminoMultisig` key.
    
    Decodes a test vector from `cosmoshub-4` (sdk@v0.42.10)

---
+++ cosmrs/src/crypto/legacy_amino.rs
@@ -0,0 +1,104 @@
+//! Legacy Amino support.
+
+use super::PublicKey;
+use crate::{prost_ext::MessageExt, proto, Any, Error, ErrorReport, Result};
+use eyre::WrapErr;
+use prost::Message;
+
+/// Legacy Amino multisig key.
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct LegacyAminoMultisig {
+    /// Multisig threshold.
+    pub threshold: u32,
+
+    /// Public keys which comprise the multisig key.
+    pub public_keys: Vec<PublicKey>,
+}
+
+impl LegacyAminoMultisig {
+    /// Protobuf [`Any`] type URL for [`LegacyAminoMultisig`].
+    pub const TYPE_URL: &'static str = "/cosmos.crypto.multisig.LegacyAminoPubKey";
+}
+
+impl From<LegacyAminoMultisig> for Any {
+    fn from(amino_multisig: LegacyAminoMultisig) -> Any {
+        let proto = proto::cosmos::crypto::multisig::LegacyAminoPubKey {
+            threshold: amino_multisig.threshold,
+            public_keys: amino_multisig
+                .public_keys
+                .into_iter()
+                .map(|pk| pk.into())
+                .collect(),
+        };
+
+        Any {
+            type_url: LegacyAminoMultisig::TYPE_URL.to_owned(),
+            value: proto
+                .to_bytes()
+                .expect("LegacyAminoPubKey serialization error"),
+        }
+    }
+}
+
+impl TryFrom<Any> for LegacyAminoMultisig {
+    type Error = ErrorReport;
+
+    fn try_from(any: Any) -> Result<LegacyAminoMultisig> {
+        LegacyAminoMultisig::try_from(&any)
+    }
+}
+
+impl TryFrom<&Any> for LegacyAminoMultisig {
+    type Error = ErrorReport;
+
+    fn try_from(any: &Any) -> Result<Self> {
+        if any.type_url != Self::TYPE_URL {
+            return Err(Error::Crypto).wrap_err_with(|| {
+                format!("invalid type URL for LegacyAminoPubKey: {}", &any.type_url)
+            });
+        }
+
+        let proto = proto::cosmos::crypto::multisig::LegacyAminoPubKey::decode(&*any.value)?;
+        let public_keys = proto
+            .public_keys
+            .into_iter()
+            .map(PublicKey::try_from)
+            .collect::<Result<Vec<_>, _>>()?;
+
+        Ok(Self {
+            threshold: proto.threshold,
+            public_keys,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::LegacyAminoMultisig;
+    use crate::{crypto::PublicKey, Any};
+    use hex_literal::hex;
+
+    #[test]
+    fn any_round_trip() {
+        let any = Any {
+            type_url: "/cosmos.crypto.multisig.LegacyAminoPubKey".to_owned(),
+            value: hex!("080312460a1f2f636f736d6f732e63727970746f2e736563703235366b312e5075624b657912230a210316eb99be27392e258ded83dc1378e507acf1bb726fa407167e709461b3a631cb12460a1f2f636f736d6f732e63727970746f2e736563703235366b312e5075624b657912230a210363deebf13d30a9840f275d01911f3e05f3fb5f88554f52b2ef534dce06b1da5912460a1f2f636f736d6f732e63727970746f2e736563703235366b312e5075624b657912230a21032e253cf8214f3d466ed296b9919821ae6681806c91b3c2063a45a8b85ce7e11512460a1f2f636f736d6f732e63727970746f2e736563703235366b312e5075624b657912230a210326ffd12bd115f260a371f2f09bf29286e4c9681c7bc109f4604c82ed82d6d23212460a1f2f636f736d6f732e63727970746f2e736563703235366b312e5075624b657912230a210343a3b485021493370286c9f4725358a3fd459576f963dcc158cb82c02276b67f").into(),
+        };
+
+        let pk = LegacyAminoMultisig::try_from(&any).unwrap();
+        assert_eq!(pk.threshold, 3);
+        assert_eq!(pk.public_keys.len(), 5);
+        assert_eq!(pk.public_keys[0].type_url(), PublicKey::SECP256K1_TYPE_URL);
+        assert_eq!(
+            pk.public_keys[0],
+            tendermint::PublicKey::from_raw_secp256k1(&hex!(
+                "0316eb99be27392e258ded83dc1378e507acf1bb726fa407167e709461b3a631cb"
+            ))
+            .unwrap()
+            .into()
+        );
+
+        // Ensure serialized key round trips
+        assert_eq!(any, Any::from(pk));
+    }
+}
--- cosmrs/src/tx/signer_info.rs
+++ cosmrs/src/tx/signer_info.rs
@@ -1,7 +1,11 @@
 //! Signer info.
 
 use super::{AuthInfo, Fee, ModeInfo, SequenceNumber, SignMode};
-use crate::{crypto::PublicKey, proto, Error, ErrorReport, Result};
+use crate::{
+    crypto::{LegacyAminoMultisig, PublicKey},
+    proto, Any, Error, ErrorReport, Result,
+};
+use eyre::WrapErr;
 
 /// [`SignerInfo`] describes the public key and signing mode of a single top-level signer.
 #[derive(Clone, Debug, Eq, PartialEq)]
@@ -10,7 +14,7 @@ pub struct SignerInfo {
     ///
     /// It is optional for accounts that already exist in state. If unset, the verifier can use the
     /// required signer address for this position and lookup the public key.
-    pub public_key: Option<PublicKey>,
+    pub public_key: Option<SignerPublicKey>,
 
     /// Signing mode.
     ///
@@ -28,7 +32,7 @@ impl SignerInfo {
     /// Create [`SignerInfo`] for a single direct signer.
     pub fn single_direct(public_key: Option<PublicKey>, sequence: SequenceNumber) -> SignerInfo {
         SignerInfo {
-            public_key,
+            public_key: public_key.map(Into::into),
             mode_info: ModeInfo::single(SignMode::Direct),
             sequence,
         }
@@ -66,6 +70,100 @@ impl From<SignerInfo> for proto::cosmos::tx::v1beta1::SignerInfo {
             public_key: signer_info.public_key.map(Into::into),
             mode_info: Some(signer_info.mode_info.into()),
             sequence: signer_info.sequence,
+        }
+    }
+}
+
+/// Signer's public key.
+#[derive(Clone, Debug, PartialEq)]
+pub enum SignerPublicKey {
+    /// Single singer.
+    Single(PublicKey),
+
+    /// Legacy Amino multisig.
+    LegacyAminoMultisig(LegacyAminoMultisig),
+
+    /// Other key types beyond the ones provided above.
+    Any(Any),
+}
+
+impl SignerPublicKey {
+    /// Get the type URL for this [`SignerPublicKey`].
+    pub fn type_url(&self) -> &str {
+        match self {
+            Self::Single(pk) => pk.type_url(),
+            Self::LegacyAminoMultisig(_) => LegacyAminoMultisig::TYPE_URL,
+            Self::Any(any) => &any.type_url,
+        }
+    }
+
+    /// Get the [`PublicKey`] for a single signer, if applicable.
+    pub fn single(&self) -> Option<&PublicKey> {
+        match self {
+            Self::Single(pk) => Some(pk),
+            _ => None,
+        }
+    }
+
+    /// Get the [`LegacyAminoMultisig`] key info, if applicable.
+    pub fn legacy_amino_multisig(&self) -> Option<&LegacyAminoMultisig> {
+        match self {
+            Self::LegacyAminoMultisig(amino_multisig) => Some(amino_multisig),
+            _ => None,
+        }
+    }
+}
+
+impl Eq for SignerPublicKey {}
+
+impl From<PublicKey> for SignerPublicKey {
+    fn from(pk: PublicKey) -> SignerPublicKey {
+        Self::Single(pk)
+    }
+}
+
+impl From<LegacyAminoMultisig> for SignerPublicKey {
+    fn from(pk: LegacyAminoMultisig) -> SignerPublicKey {
+        Self::LegacyAminoMultisig(pk)
+    }
+}
+
+impl From<SignerPublicKey> for Any {
+    fn from(public_key: SignerPublicKey) -> Any {
+        match public_key {
+            SignerPublicKey::Single(pk) => pk.into(),
+            SignerPublicKey::LegacyAminoMultisig(pk) => pk.into(),
+            SignerPublicKey::Any(any) => any,
+        }
+    }
+}
+
+impl TryFrom<Any> for SignerPublicKey {
+    type Error = ErrorReport;
+
+    fn try_from(any: Any) -> Result<Self> {
+        match any.type_url.as_str() {
+            PublicKey::ED25519_TYPE_URL | PublicKey::SECP256K1_TYPE_URL => {
+                PublicKey::try_from(any).map(Into::into)
+            }
+            LegacyAminoMultisig::TYPE_URL => LegacyAminoMultisig::try_from(any).map(Into::into),
+            _ => Ok(Self::Any(any)),
+        }
+    }
+}
+
+impl TryFrom<SignerPublicKey> for PublicKey {
+    type Error = ErrorReport;
+
+    fn try_from(public_key: SignerPublicKey) -> Result<PublicKey> {
+        match public_key {
+            SignerPublicKey::Single(pk) => Ok(pk),
+            _ => Err(Error::Crypto).wrap_err_with(|| {
+                format!(
+                    "expected `SignerPublicKey::Single`, got: {}",
+                    public_key.type_url()
+                )
+            }),
         }
     }
 }
