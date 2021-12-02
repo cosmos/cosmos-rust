a3dbc2b28fc0c117c894df478799dea9f7fbd186
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2021-12-02 14:52:32 +0000

    Updated type urls for cosmwasm msgs

--- cosmrs/src/tx/msg.rs
+++ cosmrs/src/tx/msg.rs
@@ -91,30 +91,30 @@ impl MsgProto for proto::cosmos::staking::v1beta1::MsgBeginRedelegate {
 
 #[cfg(feature = "cosmwasm")]
 impl MsgProto for proto::cosmwasm::wasm::v1::MsgStoreCode {
-    const TYPE_URL: &'static str = "/cosmwasm.wasm.v1beta1.MsgStoreCode";
+    const TYPE_URL: &'static str = "/cosmwasm.wasm.v1.MsgStoreCode";
 }
 
 #[cfg(feature = "cosmwasm")]
 impl MsgProto for proto::cosmwasm::wasm::v1::MsgInstantiateContract {
-    const TYPE_URL: &'static str = "/cosmwasm.wasm.v1beta1.MsgInstantiateContract";
+    const TYPE_URL: &'static str = "/cosmwasm.wasm.v1.MsgInstantiateContract";
 }
 
 #[cfg(feature = "cosmwasm")]
 impl MsgProto for proto::cosmwasm::wasm::v1::MsgExecuteContract {
-    const TYPE_URL: &'static str = "/cosmwasm.wasm.v1beta1.MsgExecuteContract";
+    const TYPE_URL: &'static str = "/cosmwasm.wasm.v1.MsgExecuteContract";
 }
 
 #[cfg(feature = "cosmwasm")]
 impl MsgProto for proto::cosmwasm::wasm::v1::MsgMigrateContract {
-    const TYPE_URL: &'static str = "/cosmwasm.wasm.v1beta1.MsgMigrateContract";
+    const TYPE_URL: &'static str = "/cosmwasm.wasm.v1.MsgMigrateContract";
 }
 
 #[cfg(feature = "cosmwasm")]
 impl MsgProto for proto::cosmwasm::wasm::v1::MsgUpdateAdmin {
-    const TYPE_URL: &'static str = "/cosmwasm.wasm.v1beta1.MsgUpdateAdmin";
+    const TYPE_URL: &'static str = "/cosmwasm.wasm.v1.MsgUpdateAdmin";
 }
 
 #[cfg(feature = "cosmwasm")]
 impl MsgProto for proto::cosmwasm::wasm::v1::MsgClearAdmin {
-    const TYPE_URL: &'static str = "/cosmwasm.wasm.v1beta1.MsgClearAdmin";
+    const TYPE_URL: &'static str = "/cosmwasm.wasm.v1.MsgClearAdmin";
 }
