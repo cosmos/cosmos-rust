9eccf04945de9e84ca00db288e2dc90d201f1696
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2022-05-30 14:04:42 +0100

    Added GasInfo

--- cosmrs/src/abci.rs
+++ cosmrs/src/abci.rs
@@ -3,6 +3,8 @@
 use crate::tx::Msg;
 use crate::{proto, ErrorReport, Result};
 use prost::Message;
+use serde::{Deserialize, Serialize};
+use tendermint::abci::Gas;
 
 #[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
 pub struct MsgData {
@@ -76,6 +78,35 @@ impl From<TxMsgData> for proto::cosmos::base::abci::v1beta1::TxMsgData {
     fn from(tx_msg_data: TxMsgData) -> Self {
         proto::cosmos::base::abci::v1beta1::TxMsgData {
             data: tx_msg_data.data.into_iter().map(Into::into).collect(),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, Serialize, Deserialize, Default, Eq, PartialEq)]
+struct GasInfo {
+    /// GasWanted is the maximum units of work we allow this tx to perform.
+    pub gas_wanted: Gas,
+
+    /// GasUsed is the amount of gas actually consumed.
+    pub gas_used: Gas,
+}
+
+impl TryFrom<proto::cosmos::base::abci::v1beta1::GasInfo> for GasInfo {
+    type Error = ErrorReport;
+
+    fn try_from(proto: proto::cosmos::base::abci::v1beta1::GasInfo) -> Result<GasInfo> {
+        Ok(GasInfo {
+            gas_wanted: Gas::from(proto.gas_wanted),
+            gas_used: Gas::from(proto.gas_used),
+        })
+    }
+}
+
+impl From<GasInfo> for proto::cosmos::base::abci::v1beta1::GasInfo {
+    fn from(info: GasInfo) -> Self {
+        proto::cosmos::base::abci::v1beta1::GasInfo {
+            gas_wanted: info.gas_wanted.value(),
+            gas_used: info.gas_wanted.value(),
         }
     }
 }
