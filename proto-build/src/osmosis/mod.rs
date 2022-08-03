10237b6c0a4047ecdf4a5677effdfaab85225ecb
Philipp-Sc <philipp.schluetermann@udo.edu>
2022-08-03 15:57:46 +0700

    merged osmosis-proto-build and proto-build

--- proto-build/src/osmosis/mod.rs
+++ proto-build/src/osmosis/mod.rs
@@ -3,22 +3,17 @@
 //! proto files for further compilation. This is based on the proto-compiler code
 //! in github.com/informalsystems/ibc-rs
 
+use crate::shared::*;
+use crate::shared::log_macro::info;
+
 use regex::Regex;
 use std::{
-    env,
     ffi::OsStr,
     fs::{self, create_dir_all, remove_dir_all},
     io,
     path::{Path, PathBuf},
-    process,
-    sync::atomic::{self, AtomicBool},
 };
 use walkdir::WalkDir;
-
-/// Suppress log messages
-// TODO(tarcieri): use a logger for this
-static QUIET: AtomicBool = AtomicBool::new(false);
-
 
 /// The osmosisd commit or tag to be cloned and used to build the proto files
 const OSMOSISD_REV: &str = "v8.0.0";
@@ -39,22 +34,9 @@ const TMP_BUILD_DIR: &str = "/tmp/tmp-protobuf/";
 
 /// Protos belonging to these Protobuf packages will be excluded
 /// (i.e. because they are sourced from `tendermint-proto`)
-const EXCLUDED_PROTO_PACKAGES: &[&str] = &["gogoproto", "google", "tendermint","cosmos"]; 
+const EXCLUDED_PROTO_PACKAGES: &[&str] = &["gogoproto", "google", "tendermint","cosmos"];
 
-/// Log info to the console (if `QUIET` is disabled)
-// TODO(tarcieri): use a logger for this
-macro_rules! info {
-    ($msg:expr) => {
-        if !is_quiet() {
-            println!("[info] {}", $msg)
-        }
-    };
-    ($fmt:expr, $($arg:tt)+) => {
-        info!(&format!($fmt, $($arg)+))
-    };
-}
-
-fn main() {
+pub fn main() {
     if is_github() {
         set_quiet();
     }
@@ -82,56 +64,6 @@ fn main() {
             "Rebuild protos with proto-build (osmosisd rev: {}))",
             OSMOSISD_REV
         );
-    }
-}
-
-fn is_quiet() -> bool {
-    QUIET.load(atomic::Ordering::Relaxed)
-}
-
-fn set_quiet() {
-    QUIET.store(true, atomic::Ordering::Relaxed);
-}
-
-/// Parse `--github` flag passed to `proto-build` on the eponymous GitHub Actions job.
-/// Disables `info`-level log messages, instead outputting only a commit message.
-fn is_github() -> bool {
-    env::args().any(|arg| arg == "--github")
-}
-
-fn run_cmd(cmd: &str,args: impl IntoIterator<Item = impl AsRef<OsStr>>) {
-    let stdout = if is_quiet() {
-        process::Stdio::null()
-    } else {
-        process::Stdio::inherit()
-    };
-
-    let exit_status = process::Command::new(cmd)
-        .args(args)
-        .stdout(stdout)
-        .status()
-        .expect("rm exit status missing");
-
-    if !exit_status.success() {
-        panic!("rm exited with error code: {:?}", exit_status.code());
-    }
-}
-
-fn run_git(args: impl IntoIterator<Item = impl AsRef<OsStr>>) {
-    let stdout = if is_quiet() {
-        process::Stdio::null()
-    } else {
-        process::Stdio::inherit()
-    };
-
-    let exit_status = process::Command::new("git")
-        .args(args)
-        .stdout(stdout)
-        .status()
-        .expect("git exit status missing");
-
-    if !exit_status.success() {
-        panic!("git exited with error code: {:?}", exit_status.code());
     }
 }
 
@@ -229,58 +161,6 @@ fn compile_osmosisd_proto_services(out_dir: &Path) {
     info!("=> Done!");
 }
 
-/// collect_protos walks every path in `proto_paths` and recursively locates all .proto
-/// files in each path's subdirectories, adding the full path of each file to `protos`
-///
-/// Any errors encountered will cause failure for the path provided to WalkDir::new()
-fn collect_protos(proto_paths: &[String], protos: &mut Vec<PathBuf>) {
-    for proto_path in proto_paths {
-        protos.append(
-            &mut WalkDir::new(proto_path)
-                .into_iter()
-                .filter_map(|e| e.ok())
-                .filter(|e| {
-                    e.file_type().is_file()
-                        && e.path().extension().is_some()
-                        && e.path().extension().unwrap() == "proto"
-                })
-                .map(|e| e.into_path())
-                .collect(),
-        );
-    }
-}
-
-fn copy_generated_files(from_dir: &Path, to_dir: &Path) {
-    info!("Copying generated files into '{}'...", to_dir.display());
-
-    // Remove old compiled files
-    remove_dir_all(&to_dir).unwrap_or_default();
-    create_dir_all(&to_dir).unwrap();
-
-    let mut filenames = Vec::new();
-
-    // Copy new compiled files (prost does not use folder structures)
-    let errors = WalkDir::new(from_dir)
-        .into_iter()
-        .filter_map(|e| e.ok())
-        .filter(|e| e.file_type().is_file())
-        .map(|e| {
-            let filename = e.file_name().to_os_string().to_str().unwrap().to_string();
-            filenames.push(filename.clone());
-            copy_and_patch(e.path(), format!("{}/{}", to_dir.display(), &filename))
-        })
-        .filter_map(|e| e.err())
-        .collect::<Vec<_>>();
-
-    if !errors.is_empty() {
-        for e in errors {
-            eprintln!("[error] Error while copying compiled file: {}", e);
-        }
-
-        panic!("[error] Aborted.");
-    }
-}
-
 fn copy_and_patch(src: impl AsRef<Path>, dest: impl AsRef<Path>) -> io::Result<()> {
     /// Regex substitutions to apply to the prost-generated output
     const REPLACEMENTS: &[(&str, &str)] = &[
@@ -322,4 +202,35 @@ fn copy_and_patch(src: impl AsRef<Path>, dest: impl AsRef<Path>) -> io::Result<(
     }
 
     fs::write(dest, &*contents)
+}
+
+fn copy_generated_files(from_dir: &Path, to_dir: &Path) {
+    info!("Copying generated files into '{}'...", to_dir.display());
+
+    // Remove old compiled files
+    remove_dir_all(&to_dir).unwrap_or_default();
+    create_dir_all(&to_dir).unwrap();
+
+    let mut filenames = Vec::new();
+
+    // Copy new compiled files (prost does not use folder structures)
+    let errors = WalkDir::new(from_dir)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.file_type().is_file())
+        .map(|e| {
+            let filename = e.file_name().to_os_string().to_str().unwrap().to_string();
+            filenames.push(filename.clone());
+            copy_and_patch(e.path(), format!("{}/{}", to_dir.display(), &filename))
+        })
+        .filter_map(|e| e.err())
+        .collect::<Vec<_>>();
+
+    if !errors.is_empty() {
+        for e in errors {
+            eprintln!("[error] Error while copying compiled file: {}", e);
+        }
+
+        panic!("[error] Aborted.");
+    }
 }
