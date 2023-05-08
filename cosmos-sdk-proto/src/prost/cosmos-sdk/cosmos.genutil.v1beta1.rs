074033cb55b25c82b986ff1c4f18b05b0090ffd9
Davirain <davirain.yin@gmail.com>
2023-05-08 17:06:52 +0800

    remove  cosmos-sdk-proto

--- cosmos-sdk-proto/src/prost/cosmos-sdk/cosmos.genutil.v1beta1.rs
+++
@@ -1,9 +0,0 @@
-// @generated
-/// GenesisState defines the raw genesis transaction in JSON.
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct GenesisState {
-    /// gen_txs defines the genesis transactions.
-    #[prost(bytes = "vec", repeated, tag = "1")]
-    pub gen_txs: ::prost::alloc::vec::Vec<::prost::alloc::vec::Vec<u8>>,
-}
-// @@protoc_insertion_point(module)
--- cosmos-sdk-proto/src/prost/cosmos-sdk/cosmos.orm.module.v1alpha1.rs
+++
@@ -1,7 +0,0 @@
-// @generated
-/// Module defines the ORM module which adds providers to the app container for
-/// module-scoped DB's. In the future it may provide gRPC services for interacting
-/// with ORM data.
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct Module {}
-// @@protoc_insertion_point(module)
