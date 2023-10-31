df40384c9d2f3167948e5773750d7d602ce04f53
Erlangshen219 <104747507+Erlangshen219@users.noreply.github.com>
2023-11-01 01:43:27 +0800

    Update staking.rs - Replace broken link (#443)

--- cosmrs/src/staking.rs
+++ cosmrs/src/staking.rs
@@ -1,6 +1,6 @@
 //! Staking module support
 //!
-//! <https://docs.cosmos.network/master/modules/staking/>
+//! <https://docs.cosmos.network/v0.46/modules/staking/>
 
 mod msg_begin_redelegate;
 mod msg_delegate;
