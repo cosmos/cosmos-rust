1af5c18ce6cc55cff41138376e80b18412aad2d1
Fabien Penso <fabien@pen.so>
2023-10-31 18:33:59 +0100

    Add support for uppercase bech32 (#444)
    Co-authored-by: Tony Arcieri (iqlusion) <tony@iqlusion.io>

--- cosmrs/src/base/account_id.rs
+++ cosmrs/src/base/account_id.rs
@@ -79,7 +79,12 @@ impl FromStr for AccountId {
     type Err = ErrorReport;
 
     fn from_str(s: &str) -> Result<Self> {
-        let (hrp, bytes) = bech32::decode(s).wrap_err(format!("invalid bech32: '{}'", s))?;
+        let (hrp, bytes) = if s.starts_with(|c: char| c.is_uppercase()) {
+            bech32::decode_upper(s)
+        } else {
+            bech32::decode(s)
+        }
+        .wrap_err(format!("invalid bech32: '{}'", s))?;
         Self::new(&hrp, &bytes)
     }
 }
@@ -144,6 +149,14 @@ mod tests {
     #[test]
     fn with_digit() {
         "okp41urdh3smlstyafjtyg0d606egllhwp8kvnw0d2f"
+            .parse::<AccountId>()
+            .unwrap();
+    }
+
+    /// See https://en.bitcoin.it/wiki/BIP_0173 -- UPPERCASE is a valid bech32
+    #[test]
+    fn with_uppercase() {
+        "STARS1JUME25TTJLCAQQJZJJQX9HUMVZE3VCC8QF2KWL"
             .parse::<AccountId>()
             .unwrap();
     }
